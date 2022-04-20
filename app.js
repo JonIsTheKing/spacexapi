5aea91ea84f79e6c3dc20cc58d53ccb9a9a9a149
JonIsTheKing <57835188+JonIsTheKing@users.noreply.github.com>
2022-04-20 11:21:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 const express = require('express');
-const path = require('path');
 const compController = require('./controller/controller');
 const app = express();
-const { expressCspHeader, INLINE, NONE, SELF } = require('express-csp-header');
-const cors = require("cors");
+const cors = require('cors');
 
 app.use(express.json());
 app.use(cors());
-app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, 'views'));
 
 app.use('/com/', compController);
 
